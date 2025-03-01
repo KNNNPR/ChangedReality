226195e7f19bee7d403db6ebe9233224852b2df7
Knnnpr <kirillkozloff@gmail.com>
2025-03-02 00:58:17 +0300

    Level3

---
+++ Assets/Scripts/ChildScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ChildScript : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (player == null) return;
+
+        float distanceToPlayer = player.position.x - transform.position.x;
+
+        Vector2 direction = new Vector2(player.position.x - transform.position.x, 0).normalized * speed;
+        rb.linearVelocity = direction;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Platform") Destroy(collision.gameObject);
+    }
+}
